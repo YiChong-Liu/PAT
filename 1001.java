091849b3ec0035b4aa67b72a6cff095ca3c2abec
liuyichong <jiuliuwangdao@foxmail.com>
2019-08-14 17:09:44 +0800

    提交题目1001代码

---
+++ 1001.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int num;
+		while(sc.hasNext()) {
+			num=sc.nextInt();
+			int all=0;
+			while(num!=1) {
+				all++;
+				num=change(num);
+			}
+			System.out.println(all);
+		}
+	}
+	
+	public static int change(int num) {
+		if(num%2==0)num=num/2;
+		else
+			num=(3*num+1)/2;
+		return num;
+	}
+}
