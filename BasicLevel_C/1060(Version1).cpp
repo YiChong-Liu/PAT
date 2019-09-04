d3e5457d9e5951773d8ff41731048b5d3e701ce0
liuyichong <jiuliuwangdao@foxmail.com>
2019-09-04 15:52:27 +0800

    update

---
+++ BasicLevel_C/1060(Version1).cpp
@@ -0,0 +1,33 @@
+﻿/*author:moonlake23
+question1060:爱丁顿数*/
+
+/*时间复杂度为O(n^2)
+提交时有一个点可能tle或段错误,
+还是简化一下为好*/
+
+#include<stdio.h>
+int main() {
+	int days;
+	scanf("%d", &days);
+	int acres[100001];
+	for (int i = 0; i < days; i++) {
+		scanf("%d", &acres[i]);
+	}
+
+	int flag = 1;
+	int E, finalE=0;
+	for (E = 0;; E++) {
+		int mark = 0;
+		if (!flag)break;
+		for (int i = 0; i < days; i++) {
+			if (acres[i] >E)mark++;
+		}
+		if (mark < E)flag = 0;
+		else
+			finalE = E;
+	}
+
+	printf("%d\n", finalE);
+	return 0;
+}
+
--- BasicLevel_C/1060.cpp
+++ BasicLevel_C/1060.cpp
@@ -1,29 +1,35 @@
 ﻿/*author:moonlake23
 question1060:爱丁顿数*/
 
-#include<stdio.h>
+/*分析：从下标1开始存储n天的公里数
+在数组a中，对n个数据从大到小排序，
+i表示了骑车的天数，那么满足
+a[i] > i的最大值即为所求*/
+
+/*时间复杂度降到了O(n)*/
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int acres[1000001];
+
+bool cmp(int a, int b) {
+	return a > b;
+}
 int main() {
 	int days;
 	scanf("%d", &days);
-	int acres[100001];
-	for (int i = 0; i < days; i++) {
+	int i, j;
+	for (i = 0; i < days; i++) {
 		scanf("%d", &acres[i]);
 	}
-
-	int flag = 1;
-	int E, finalE=0;
-	for (E = 0;; E++) {
-		int mark = 0;
-		if (!flag)break;
-		for (int i = 0; i < days; i++) {
-			if (acres[i] >E)mark++;
-		}
-		if (mark < E)flag = 0;
-		else
-			finalE = E;
+	sort(acres,acres+days, cmp);
+	int E = 0;
+	while (E < days&&acres[E]>E + 1) {
+		E++;       
 	}
+	printf("%d\n", E);
 
-	printf("%d\n", finalE);
 	return 0;
 }
+
 
