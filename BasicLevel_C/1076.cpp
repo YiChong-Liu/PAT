3d7d329f73bbc3b45f6536d51b3cb108ecdff25c
liuyichong <jiuliuwangdao@foxmail.com>
2019-09-05 11:19:31 +0800

    update

---
+++ BasicLevel_C/1076.cpp
@@ -0,0 +1,19 @@
+﻿/*author:moonlake23
+question1076:wifi密码*/
+
+/*此题直接挨个读取
+就行了，非常简单*/
+#include<stdio.h>
+int main()
+{
+	char string[4];
+	while (scanf("%s", string) != EOF) {
+		if (string[2] == 'T')
+			printf("%d", string[0] - 64);
+	}
+	printf("\n");
+	return 0;
+}
+
+
+
---
+++ BasicLevel_C/1082.cpp
@@ -0,0 +1,39 @@
+﻿/*author:moonlake23
+question1082:射击比赛*/
+
+typedef struct {
+	char ID[5];
+	int x;
+	int y;
+}Player;
+
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+int main() {
+	int cases;
+	scanf("%d", &cases);
+	Player group[10001];
+	for (int i = 0; i < cases; i++) {
+		scanf("%s %d %d", group[i].ID, &group[i].x, &group[i].y);
+	}
+
+	double max = 0.0,min=200.0,distance;
+	char IDmax[5], IDmin[5];
+	for (int i = 0; i < cases; i++) {
+		distance = sqrt(group[i].x*group[i].x + group[i].y*group[i].y);
+		if (distance > max) {
+			max = distance;
+			strcpy(IDmax, group[i].ID);
+		}
+		if (distance < min) {
+			min = distance;
+			strcpy(IDmin, group[i].ID);
+		}
+	}
+	printf("%s %s\n", IDmin, IDmax);
+	return 0;
+}
+
+
+
