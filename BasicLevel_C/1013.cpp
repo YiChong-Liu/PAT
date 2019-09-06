ad090cdf509c6392f59fc8d7332761a583ae02ff
liuyichong <jiuliuwangdao@foxmail.com>
2019-09-06 20:34:10 +0800

    update

---
+++ BasicLevel_C/1013.cpp
@@ -0,0 +1,38 @@
+﻿/*author:moonlake23
+question1013:数素数*/
+
+#include<stdio.h>
+#include<math.h>
+
+int IsPrimenum(int num) {
+	int flag = 1;
+	if (num == 0 || num == 1)return 0;
+	else {
+		for (int i = 2; i*i<=num; i++) {
+			if (num%i == 0)flag = 0;
+		}
+		return flag;
+	}
+}
+
+int main() {
+	int start, end;
+	scanf("%d %d", &start, &end);
+	int sum = 0;
+	int num[10001], mark = 0;
+	for (int i = 1;; i++) {
+		if (IsPrimenum(i) == 1) {
+			sum++;
+			if (sum >= start&&sum <= end)
+				num[mark++] = i;
+		}
+		if (sum > end)break;
+	}
+	for (int i = 0; i < mark; i++) {
+		if ((i + 1) % 10 == 0 && i != mark - 1)printf("%d\n", num[i]);
+		else if (i == mark - 1)printf("%d", num[i]);
+		else
+			printf("%d ", num[i]);
+	}
+	return 0;
+}
---
+++ BasicLevel_C/1086.cpp
@@ -0,0 +1,21 @@
+﻿/*author:moonlake23
+question1086:就不告诉你*/
+
+#include<stdio.h>
+#include<math.h>
+int main() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	int result = a*b, sum = 0;
+	int num[101], mark = 0;
+	while (result) {
+		num[mark++] = result % 10;
+		result /= 10;
+	}
+
+	for (int i = 0; i < mark; i++) {
+		sum += num[i] * pow(10, mark - i - 1);
+	}
+	printf("%d\n", sum);
+	return 0;
+}
