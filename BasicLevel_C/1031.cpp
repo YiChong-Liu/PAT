efe06995ed0d4b73dc550bfe6415612065f7d709
liuyichong <jiuliuwangdao@foxmail.com>
2019-09-07 17:10:10 +0800

    update

---
+++ BasicLevel_C/1031.cpp
@@ -0,0 +1,45 @@
+﻿/*author:moonlake23
+question1031:查验身份证*/
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	char num[101][20];
+	char num2[101][20];
+	int	weight[17] = { 7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2 };
+	char code[11] = { '1','0','X','9','8','7','6','5','4','3','2' };
+	int i, j, m, k = 0;
+	for (i = 0; i<n; i++)
+		scanf("%s", num[i]);
+	for (i = 0; i<n; i++)
+	{
+		int sum = 0;
+		for (j = 0; j<17; j++)
+		{
+			if (num[i][j]>'9' || num[i][j]<'0')
+				sum += 10;
+			sum += ((num[i][j] - '0')*weight[j]);
+		}
+		if (code[sum % 11] != num[i][17])
+		{
+			strcpy(num2[k++], num[i]);
+		}
+	}
+	if (k == 0)
+		printf("All passed\n");
+	else
+	{
+		for (i = 0; i<k; i++)
+		{
+			printf("%s", num2[i]);
+			printf("\n");
+		}
+	}
+	return 0;
+}
+
+
+
+
---
+++ BasicLevel_C/1065(Version1).cpp
@@ -0,0 +1,62 @@
+﻿/*author:moonlake23
+question1065:单身狗*/
+
+/*思路一：
+疯狂超时哈哈哈*/
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+typedef struct {
+	int husband=0;
+	int wife=0;
+}CoupleID;
+
+
+int main() {
+	int couples;
+	scanf("%d", &couples);
+	CoupleID cp[50001];
+	for (int i = 0; i < couples; i++) {
+		scanf("%d %d", &cp[i].husband, &cp[i].wife);
+	}
+
+	int numbers;
+	scanf("%d", &numbers);
+	int guests[10001] = { 0 };
+	for (int i = 0; i < numbers; i++) {
+		scanf("%d", &guests[i]);
+	}
+
+	int left = 0;//落单人数
+	for (int j = 0; j < couples; j++) {
+		for (int i = 0; i < numbers; i++)
+		{
+			if (guests[i] == cp[j].husband) 
+			{
+				for (int k = 0; k < numbers; k++) {
+					if (guests[k] == cp[j].wife&&k != i)
+						guests[k] = guests[i] = 0;
+				}
+			}
+		}
+	}
+
+	for (int j = 0; j < numbers; j++) {
+		if (guests[j] != 0)left++;
+	}
+
+	printf("%d\n", left);
+	sort(guests, guests + numbers);
+	for (int j = 0; j < numbers; j++) {
+		if (guests[j] != 0) {
+			left--;
+			if (left >0)printf("%d ", guests[j]);
+			else
+				printf("%d\n", guests[j]);
+		}
+	}
+	return 0;
+}
+
+
