048378ffba64fe17b93b291ebc7330d2f8c19848
liuyichong <jiuliuwangdao@foxmail.com>
2019-09-13 19:16:38 +0800

    update

---
+++ BasicLevel_C/1007.cpp
@@ -0,0 +1,24 @@
+﻿/*author:moonlake23
+question1007:素数对猜想*/
+
+#include<stdio.h>
+int IsPrimenum(int num) {
+	int flag = 1;
+	if (num == 0 || num == 1)return 1;
+	for (int i = 2; i*i <= num; i++) {
+		if (num%i == 0)flag = 0;
+	}
+	return flag;
+}
+
+int main() {
+	int max,mark = 0;
+	scanf("%d", &max);
+	for (int i = 2; i <= max-2; i++) {
+		if (IsPrimenum(i) == 1 && IsPrimenum(i + 2) == 1) {
+			mark++;
+		}
+	}
+	printf("%d\n", mark);
+	return 0;
+}
---
+++ BasicLevel_C/1021.cpp
@@ -0,0 +1,33 @@
+﻿/*author:moonlake23
+question1021:个位数统计*/
+
+#include<stdio.h>
+#include<string.h>
+typedef struct {
+	char key;
+	int cases;
+}Answer;
+int main() {
+	char num[1001];
+	gets_s(num);
+	int length = strlen(num);
+	Answer obj[11];
+
+	for (int i = 48; i <= 58; i++) {
+		obj[i - 48].key = i;
+		obj[i - 48].cases = 0;
+	}
+
+	for (int i = 0; i < length; i++) {
+		for (int j = 0; j < 11; j++) {
+			if (obj[j].key == num[i])obj[j].cases++;
+		}
+	}
+
+	for (int i = 0; i < 11; i++)
+		if (obj[i].cases != 0) {
+			printf("%c:%d\n", obj[i].key, obj[i].cases);
+		}
+
+	return 0;
+}
